895da1eea9529b571a3b6fdb828ff07b0e46d090
shadowplay71293 <mando71293@gmail.com>
2017-03-13 22:05:48 -0700

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            long resultado;
+            Program objeto = new Program();
+
+            Console.WriteLine("Introduce la palabra");
+
+            s= Console.ReadLine();
+
+
+            resultado = Program.hashfunction(s);
+
+            Console.WriteLine(resultado);
+            Console.Read();
+
+            
+        }
+        public static Int64 hashfunction(string s) {
+
+             Int64 h;
+            string letters;
+            int i;
+            int index;
+            long inc;
+
+            h = 7;
+            letters = "acdegilmnoprstuw";
+
+            for(i=0; i<s.Length;i++) {
+                inc = h * 37;
+                index = letters.IndexOf(s[i]);
+                h = inc + index;
+            }
+
+            return h;
+        }
+    }
+}
---
+++ ConsoleApplication3/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConsoleApplication3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string palabra = "leepadg";
+            long expectedResult = 680131659347;
+            long result;
+            result=Program.hashfunction(palabra);
+
+            Assert.AreEqual(result,expectedResult);
+
+        }
+  
+    }
+}
